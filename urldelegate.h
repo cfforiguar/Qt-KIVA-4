1a2b994f2cf883387c74578a13cef51d7728095a
carlos <none@none>
2017-08-25 21:42:39 -0500

    urldelegate.*:   -Se incorpora el enlace al sitio de la página de Hessel cuando se hace click    en el término correspondiente en la primera columna del itape5.

--- urldelegate.h
+++ urldelegate.h
@@ -3,6 +3,7 @@
 
 #include <QAbstractItemDelegate>
 #include <QtWidgets>
+#include <QList>
 //Pinta, genera y abre las URL de las palabras clave de KIVA
 //Basado en los ejemplos "spinboxdelegate" y "stardelegate"
 // y basado en  https://stackoverflow.com/questions/2375763/how-to-open-an-url-in-a-qtableview
@@ -10,13 +11,16 @@ class UrlDelegate : public QAbstractItemDelegate
 {
      Q_OBJECT
 public:
-    UrlDelegate(QObject * parent=0):QAbstractItemDelegate(parent){}
+    UrlDelegate(QObject * parent=0);
     void paint(QPainter *painter, const QStyleOptionViewItem &option,
                const QModelIndex &index) const override;
     QSize sizeHint(const QStyleOptionViewItem & option, const QModelIndex & index) const override;
     bool editorEvent(QEvent *event, QAbstractItemModel *model,
                                 const QStyleOptionViewItem &option, const QModelIndex &index) override;
-
+    void buildKwordLinks();
+private:
+    QList<QString> listLinks;
+    QList<QString> listKwords;
 };
 
 #endif // URLDELEGATE_H
